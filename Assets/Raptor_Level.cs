05ff05e5ec81ea3d95c44d3841689844b198d07c
edwingl <Bone17@Maekar.local>
2019-02-20 22:57:25 -0800

    Tool inventory and more sprites.

---
+++ Assets/Raptor_Level.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Raptor_Level : MonoBehaviour
+{
+   public void LoadRaptorScene()
+    {
+        SceneManager.LoadScene("RaptorDiagnose");
+    }
+}
