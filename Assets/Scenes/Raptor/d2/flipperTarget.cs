de4fc4dc6a5629aa343ff367bcc4cf2cf3e55ffe
edwingl <Bone17@Maekar.local>
2019-03-18 00:41:47 -0700

    more animations/transitions, reorganized diagnosis treatment, stopped at glucose

---
+++ Assets/Scenes/Raptor/d2/flipperTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class flipperTarget : EventTrigger
+{
+    //used to check if magnifying glass has been over flipper
+    public static bool flipper = false;
+
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        //if the user is dragging the magnifying glass
+        if (magDrag.itemBeingDragged != null)
+        {
+            flipper = true;
+
+        }
+
+    }
+}
---
+++ Assets/Scenes/Raptor/d2/magDrag.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class magDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    //game object that is being dragged
+    public static GameObject itemBeingDragged;
+    //stores the start position of the game object
+    Vector3 startPosition;
+    //stores the parent of the game object
+    Transform startParent;
+    //references clipboard game object
+    public GameObject clip_board;
+
+    //references instructions game object
+    public GameObject Instructions;
+
+    //plays clip board animation
+    public void play_clipboard()
+    {
+        Animator anim = clip_board.GetComponent<Animator>();
+        anim.SetBool("play", true);
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //sets item being dragged to the current game object
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        //sets the start parent of the object
+        startParent = transform.parent;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        transform.SetParent(transform.root);
+       
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //position of game object follows mouse position 
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+
+        //snaps object back to original slot if not over the target slot 
+        if (transform.parent == startParent || transform.parent == transform.root)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+        }
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    public void Update()
+    {
+        if(mouthTarget.mouth && flipperTarget.flipper)
+        {
+            //hides the instructions panel
+            Instructions.gameObject.SetActive(false);
+            play_clipboard();
+        }
+    }
+
+}
