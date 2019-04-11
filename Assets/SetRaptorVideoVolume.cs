1269641ec8de74b871e050ae79b14e13ff7b7961
Daniel Nakasone <dnakason@uci.edu>
2019-04-11 10:10:36 -0700

    Adding audio and scenes, along with the files that were added to the master branch

---
+++ Assets/SetRaptorVideoVolume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetRaptorVideoVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+
+    public void setLevel (float sliderValue)
+    {   // sets audio level
+        mixer.SetFloat("RaptorAudioVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
